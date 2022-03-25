2aafefc137a836f885284bc49f9e703fe6d31e6a
carneirogp <66340226+carneirogp@users.noreply.github.com>
2022-03-24 21:39:37 -0300

    push1

---
+++ EX02/Principal.java
@@ -0,0 +1,48 @@
+package EX02;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		
+		DAO dao = new DAO();
+		
+		dao.conectar();
+
+		
+		//Inserir um elemento na tabela
+		Carro carro = new Carro(11, "pablo", "pablo",'M');
+		if(dao.inserirCarro(carro) == true) {
+			System.out.println("Inserção com sucesso -> " + carro.toString());
+		}
+		
+		//Mostrar usuários do sexo masculino		
+		System.out.println("==== Mostrar usuários do sexo masculino === ");
+		Carro[] carros = dao.getcarrosMasculinos();
+		for(int i = 0; i < usuarios.length; i++) {
+			System.out.println(usuarios[i].toString());
+		}
+
+		//Atualizar usuário
+		carro.setSenha("nova senha");
+		dao.atualizarCarro(carro);
+
+		//Mostrar usuários do sexo masculino
+		System.out.println("==== Mostrar usuários === ");
+		usuarios = dao.getUsuarios();
+		for(int i = 0; i < usuarios.length; i++) {
+			System.out.println(usuarios[i].toString());
+		}
+		
+		//Excluir usuário
+		dao.excluirCarro(carro.getCodigo());
+		
+		//Mostrar usuários
+		usuarios = dao.getUsuarios();
+		System.out.println("==== Mostrar usuários === ");		
+		for(int i = 0; i < usuarios.length; i++) {
+			System.out.println(usuarios[i].toString());
+		}
+		
+		dao.close();
+	}
+}
